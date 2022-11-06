79071bbd878c6199338b870bc22f2ef61572346c
Kruger <jsgwtwo@gmail.com>
2022-11-06 13:55:05 -0500

    Adding call

--- LazCalls.lua
+++ LazCalls.lua
@@ -3,6 +3,7 @@ local LSM = LibStub("LibSharedMedia-3.0")
 local colorString = "FF52FF0E"
 
 LSM:Register("sound", "|c" .. colorString .. "Move To Diamond|r", [[Interface\Addons\LazCalls\sound\MoveToDiamond.ogg]])
+LSM:Register("sound", "|c" .. colorString .. "Move To Diamond_02|r", [[Interface\Addons\LazCalls\sound\MoveToDiamond.ogg]])
 LSM:Register("sound", "|c" .. colorString .. "CN01-Blind Swipe|r", [[Interface\Addons\LazCalls\sound\CN01-Blind Swipe.ogg]])
 LSM:Register("sound", "|c" .. colorString .. "CN01-Earsplitting Screech|r", [[Interface\Addons\LazCalls\sound\CN01-Earsplitting Screech.ogg]])
 LSM:Register("sound", "|c" .. colorString .. "CN01-Echoing Screech|r", [[Interface\Addons\LazCalls\sound\CN01-Echoing Screech.ogg]])
