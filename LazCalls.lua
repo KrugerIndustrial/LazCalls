0be12a6a7c986612fc0bf842b31e905aae82391b
Joe Snevely <jsnevely@gmail.com>
2022-11-06 09:56:17 -0500

    version bump

--- LazCalls.lua
+++ LazCalls.lua
@@ -1,6 +1,5 @@
 local LSM = LibStub("LibSharedMedia-3.0") 
 
-
 LSM:Register("sound", "|cFFFFD700Move To Diamond|r", [[Interface\Addons\LazCalls\sound\MoveToDiamond.ogg]])
 LSM:Register("sound", "|cFFFFD700CN01-Blind Swipe|r", [[Interface\Addons\LazCalls\sound\CN01-Blind Swipe.ogg]])
 LSM:Register("sound", "|cFFFFD700CN01-Earsplitting Screech|r", [[Interface\Addons\LazCalls\sound\CN01-Earsplitting Screech.ogg]])
