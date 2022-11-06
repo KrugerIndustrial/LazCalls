0c6a2e091debe8116d6ca3bc912fe81b69f1de19
Joe Snevely <jsnevely@gmail.com>
2022-11-06 10:43:12 -0500

    color update

--- LazCalls.lua
+++ LazCalls.lua
@@ -1,7 +1,9 @@
 local LSM = LibStub("LibSharedMedia-3.0") 
 
-LSM:Register("sound", "|cFFFFD700Move To Diamond|r", [[Interface\Addons\LazCalls\sound\MoveToDiamond.ogg]])
-LSM:Register("sound", "|cFFFFD700CN01-Blind Swipe|r", [[Interface\Addons\LazCalls\sound\CN01-Blind Swipe.ogg]])
-LSM:Register("sound", "|cFFFFD700CN01-Earsplitting Screech|r", [[Interface\Addons\LazCalls\sound\CN01-Earsplitting Screech.ogg]])
-LSM:Register("sound", "|cFFFFD700CN01-Echoing Screech|r", [[Interface\Addons\LazCalls\sound\CN01-Echoing Screech.ogg]])
-LSM:Register("sound", "|cFFFFD700CN01-Echolocation|r", [[Interface\Addons\LazCalls\sound\CN01-Echolocation.ogg]])
+local colorString = "FF52FF0E"
+
+LSM:Register("sound", "|c" .. colorString .. "Move To Diamond|r", [[Interface\Addons\LazCalls\sound\MoveToDiamond.ogg]])
+LSM:Register("sound", "|c" .. colorString .. "CN01-Blind Swipe|r", [[Interface\Addons\LazCalls\sound\CN01-Blind Swipe.ogg]])
+LSM:Register("sound", "|c" .. colorString .. "CN01-Earsplitting Screech|r", [[Interface\Addons\LazCalls\sound\CN01-Earsplitting Screech.ogg]])
+LSM:Register("sound", "|c" .. colorString .. "CN01-Echoing Screech|r", [[Interface\Addons\LazCalls\sound\CN01-Echoing Screech.ogg]])
+LSM:Register("sound", "|c" .. colorString .. "CN01-Echolocation|r", [[Interface\Addons\LazCalls\sound\CN01-Echolocation.ogg]])
